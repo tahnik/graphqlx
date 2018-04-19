06068fa8a89ad3a18c518c0f2ec9fb305eb95bbf
Tahnik Mustasin <tahnik@live.co.uk>
2018-04-19 19:03:53 +0100

    Added comments for dir.ml

--- lib/ocaml/dir/dir.ml
+++ lib/ocaml/dir/dir.ml
@@ -1,12 +1,20 @@
-(** [dir_is_empty dir] is true, if [dir] contains no files except
+(**
+ * Name: Dir
+ *
+ * Responsible for reading the filenames of a given directory
+ *)
+
+(**
+ * [dir_is_empty dir] is true, if [dir] contains no files except
  * "." and ".."
  *)
- let dir_is_empty dir =
+let dir_is_empty dir =
   Array.length (Sys.readdir dir) = 0
 
-(** [dir_contents] returns the paths of all regular files that are
+(**
+ * [dir_contents] returns the paths of all regular files that are
  * contained in [dir]. Each file is a path starting with [dir].
-  *)
+ *)
 let dir_contents dir =
   let rec loop result = function
     | f::fs when Sys.is_directory f ->
