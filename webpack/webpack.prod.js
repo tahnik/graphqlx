23995d907760f52ef70b7ec18feca3d16040905b
Tahnik Mustasin <tahnik@live.co.uk>
2018-04-20 20:06:42 +0100

    Optimized webpack

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,13 +1,25 @@
 const path = require("path");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, "../src/main.js"),
-  mode: 'production',
+  mode: "production",
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "graphqlx.js",
     library: "graphqlx",
     libraryTarget: "umd"
   },
-  target: "node"
+  target: "node",
+  plugins: [
+    new UglifyJsPlugin({
+      sourceMap: true,
+      parallel: true,
+      uglifyOptions: {
+        compress: {
+          drop_console: true,
+        }
+      }
+    })
+  ]
 };
